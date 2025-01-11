92d9e6b242d1263708cd2ebfcb81015b09fad77c
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-01-11 14:32:05 +0200

    work

--- js/task-1_task.js
+++ js/task-1_task.js
@@ -1,1 +1,14 @@
-const Account = 
+function Account(login, email) {
+  this.login = login;
+  this.email = email;
+}
+
+Account.prototype.getInfo = function () {
+  console.log(`You login: ${this.login}, email: ${this.email}`);
+};
+
+const mango = new Account("Mangozedog", "mango@dog.woof");
+mango.getInfo();
+
+const poly = new Account("Poly", "poly@mail.com");
+poly.getInfo();
